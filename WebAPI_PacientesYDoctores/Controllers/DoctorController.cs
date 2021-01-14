9ae85be092e83820721e3c9593553af4a6320a2a
Diego Rodriguez Nava <diego@edugraph.us>
2021-01-14 18:12:51 -0500

    Doctor Controller

---
+++ WebAPI_PacientesYDoctores/Controllers/DoctorController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI_PacientesYDoctores.Entidades;
+
+namespace WebAPI_PacientesYDoctores.Controllers
+{
+    public class DoctorController:ControllerBase
+    {
+        private readonly ApiDrYPacienteDBContext dbcontext;
+
+        public DoctorController(ApiDrYPacienteDBContext dbcontext)
+        {
+            this.dbcontext = dbcontext;
+        }
+
+        //Acciones
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Doctor>>> GetDoctores()
+        {
+            return await dbcontext.Doctores.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Doctor>> GetDoctores(long id)
+        {
+            var doctor = await dbcontext.Doctores.FindAsync(id);
+            if (doctor == null)
+                return NotFound();
+            return doctor;
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Doctor>> GetDoctor(long id)
+        {
+            var doctor = await dbcontext.Doctores.FindAsync(id);
+            if (doctor == null)
+                return NotFound();
+            return doctor;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostDoctor(Doctor doctor)
+        {
+            await dbcontext.Doctores.AddAsync(doctor);
+            await dbcontext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetDoctor), new { id = doctor.Id }, doctor);
+
+        }
+    }
+}
